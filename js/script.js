6ccd076ba49c6bfac015d8fe2ea49cb8c19e9282
Dimych <example@gmail.com>
2020-07-01 23:19:51 +0300

    06.02

--- js/script.js
+++ js/script.js
@@ -16,4 +16,18 @@ console.log(something);
 let und;
 console.log(und);
 
+const obj = {
+  name : 'John',
+  age : 25,
+  isMarried : false
+};
+
+console.log(obj);
+
+console.log(obj.name);
+console.log(obj['name']);
+
+
+
+
 
