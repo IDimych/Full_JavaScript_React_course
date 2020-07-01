f9472434bd35e279f1f656cdd3670bd0ae37ca55
Dimych <example@gmail.com>
2020-07-02 00:24:54 +0300

    07.02

--- js/script.js
+++ js/script.js
@@ -2,12 +2,22 @@
 
 // alert('Hello');
 
-const result = confirm("Are you here?");
-console.log(result);
+// const result = confirm("Are you here?");
+// console.log(result);
 
-const answer = +prompt("Вам есть 18?", "18");
-console.log(answer);
-console.log(typeof (answer));
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer);
+// console.log(typeof (answer));
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как вашa фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
+
+
 
 
 
