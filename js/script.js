db95b9ac2897f419f5c1e55de40e75dfc4a075a4
Dimych <example@gmail.com>
2020-07-02 00:10:33 +0300

    07.01

--- js/script.js
+++ js/script.js
@@ -1,35 +1,14 @@
 "use strict";
 
-let number = 4.6;
+// alert('Hello');
 
-console.log(4/0);
-console.log(-4/0);
-console.log('string' * 9);
+const result = confirm("Are you here?");
+console.log(result);
 
-const persone = 'Alex';
-const personeN = '5';
+const answer = +prompt("Вам есть 18?", "18");
+console.log(answer);
+console.log(typeof (answer));
 
-const bool = true;
 
-console.log(something);
-
-let und;
-console.log(und);
-
-const obj = {
-  name : 'John',
-  age : 25,
-  isMarried : false
-};
-
-console.log(obj);
-
-console.log(obj.name);
-console.log(obj['name']);
-
-let arr = ['plum.png', 'orange.jpg', 6,  'apple.bmp', {}, []];
-
-console.log(arr);
-console.log(arr[1]);
 
 
