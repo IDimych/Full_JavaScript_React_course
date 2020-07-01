039d4e3f7ca2de858af2e53b321a5dcdcb9f26cf
Dimych <example@gmail.com>
2020-07-01 23:29:39 +0300

    06.03

--- js/script.js
+++ js/script.js
@@ -27,7 +27,9 @@ console.log(obj);
 console.log(obj.name);
 console.log(obj['name']);
 
+let arr = ['plum.png', 'orange.jpg', 6,  'apple.bmp', {}, []];
 
-
+console.log(arr);
+console.log(arr[1]);
 
 
