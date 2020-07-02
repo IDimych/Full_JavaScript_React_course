840bc231d93505d73c9ce6aa9ca795741e110526
Dimych <example@gmail.com>
2020-07-02 17:43:21 +0300

    13.02

--- js/script.js
+++ js/script.js
@@ -2,8 +2,17 @@
 
 if (4 == 9) {
   console.log('Ok!');
+} else {
+  console.log('Error!');
 }
 
-if (4 == 4) {
+const num = 50;
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Много');
+} else {
   console.log('Ok!');
 }
+
