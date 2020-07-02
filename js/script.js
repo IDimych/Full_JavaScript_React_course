db10a16c6ab1fa0877b0800fca75bb32d3b29067
Dimych <example@gmail.com>
2020-07-02 17:55:32 +0300

    13.04

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 'use strict';
 
-if (4 == 9) {
-  console.log('Ok!');
-} else {
-  console.log('Error!');
-}
-
 const num = 50;
 
-if (num < 49) {
-    console.log('Error');
-} else if (num > 100) {
-    console.log('Много');
-} else {
-  console.log('Ok!');
-}
+switch (num) {
+  case 49:
+    console.log('Неверно');
+    break;
+  case 100:
+    console.log('Неверно');
+    break;
+  case 50:
+    console.log('В точку');
+    break;
+  default:
+    console.log('Не в этот раз');
+    break;
+};
 
-(num === 50) ? console.log('Ok!') : console.log('Error!');
 
