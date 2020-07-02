706afe5d6cc8589798c9132f6a084ab94d5ff325
Dimych <example@gmail.com>
2020-07-02 18:27:39 +0300

    14.01

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 'use strict';
 
-const num = 50;
+let num = 50;
 
-switch (num) {
-  case 49:
-    console.log('Неверно');
-    break;
-  case 100:
-    console.log('Неверно');
-    break;
-  case 50:
-    console.log('В точку');
-    break;
-  default:
-    console.log('Не в этот раз');
-    break;
-};
+while(num <= 55) {
+  console.log(num);
+  num++;
+}
 
+num = 50;
+
+do {
+  console.log(num);
+  num++;
+}
+while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+  console.log(i);
+}
 
