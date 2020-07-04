db57246ebb9094969baabac3d73ec27aa4c33f81
Dimych <example@gmail.com>
2020-07-04 14:00:46 +0300

    20.03

--- js/script.js
+++ js/script.js
@@ -20,13 +20,17 @@ console.log(options.name);
 
 //console.log(options);
 
+let counter = 0;
 for (let key in options) {
   if (typeof (options[key]) === 'object') {
     for (let i in options[key]){
-      console.log(`Свойство ${i}, имеет значение ${options[key][i]}`);
+      console.log(`\tСвойство ${i}, имеет значение ${options[key][i]}`);
+      counter ++;
     }
   } else {
     console.log(`Свойство ${key}, имеет значение ${options[key]}`);
+    counter ++;
   }
 }
+console.log(counter);
 
