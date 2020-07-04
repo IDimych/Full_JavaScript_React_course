d508196ff329690a37a199d7d65a072826987351
Dimych <example@gmail.com>
2020-07-05 01:04:52 +0300

    28.02

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ console.log(btns[0]);
 const circles = document.getElementsByClassName('circle');
 console.log(circles);
 
+const hearts = document.querySelectorAll('.heart');
+console.log(hearts);
+
+hearts.forEach(item => {
+  console.log(item);
+});
+
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
