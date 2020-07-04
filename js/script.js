eb097f4d415356b70a5d3246376ea2784df06c10
Dimych <example@gmail.com>
2020-07-05 01:57:53 +0300

    29.03

--- js/script.js
+++ js/script.js
@@ -16,6 +16,19 @@ box.style.cssText = `background-color: blue; width: ${num}px`;
 btns[1].style.borderRadius = '100%';
 circles[0].style.backgroundColor = 'red';
 
-for (let i =0; i < hearts.length; i++) {
-  hearts[i].style.backgroundColor = 'blue';
-}
+// for (let i =0; i < hearts.length; i++) {
+//   hearts[i].style.backgroundColor = 'blue';
+// }
+
+hearts.forEach(item => {
+  item.style.backgroundColor = 'blue';
+});
+
+const div = document.createElement('div');
+// const text = document.createTextNode('Тут был я');
+
+div.classList.add('black');
+
+// document.body.append(div);
+
+document.querySelector('.wrapper').append(div);
