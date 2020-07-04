95fc521568f7bafc45b108bd34b85647986c5c8e
Dimych <example@gmail.com>
2020-07-05 00:49:12 +0300

    28.01

--- js/script.js
+++ js/script.js
@@ -1,51 +1,11 @@
 'use strict';
 
-// 1)
-console.log(typeof(String(null)));
-console.log(typeof(String(4)));
+const box = document.getElementById('box');
+console.log(box);
 
-// 2)
-console.log(typeof(5 + ''));
+const btns = document.getElementsByTagName('button');
+console.log(btns[0]);
 
-const num = 5;
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
 
-console.log('https://vk.com/catalog/' + num);
-
-const fontSize = 26 + 'px';
-
-// To Number
-
-// 1)
-console.log(typeof(Number('4')));
-
-// 2)
-console.log(typeof(Number(+'5')));
-
-// 3)
-console.log(typeof(parseInt('15 px', 10)));
-
-//let answ = +prompt("Hello", "");
-
-// To boolean
-
-// 0, '',  null, undefained, NaN - false
-
-// 1)
-
-let switcher = null;
-
-if (switcher) {
-  console.log('Working...');
-}
-
-switcher = 1;
-
-if (switcher) {
-  console.log('Working...');
-}
-
-// 2)
-console.log(typeof(Boolean('4')));
-
-// 3)
-console.log(typeof(!!'44444'));
