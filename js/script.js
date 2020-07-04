72bb840d74fa271ee7249bfdbe4e55c7b669fb0d
Dimych <example@gmail.com>
2020-07-04 17:51:26 +0300

    23.02

--- js/script.js
+++ js/script.js
@@ -8,4 +8,25 @@ console.log(typeof(strObj));
 
 console.dir([1, 2, 3]);
 
- 
+ const soldier = {
+   health: 400,
+   armor: 100,
+   sayHello: function() {
+     console.log('Hello');
+   }
+ };
+
+ const jonh = {
+   health: 100
+ };
+
+//  jonh.__proto__ = soldier;
+
+Object.setPrototypeOf(jonh, soldier);
+
+
+ console.log(jonh.armor);
+ jonh.sayHello();
+
+
+
