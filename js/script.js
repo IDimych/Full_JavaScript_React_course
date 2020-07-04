0a9b7fc7b2e05dd2b7dd9be7350717a528e481dc
Dimych <example@gmail.com>
2020-07-04 15:17:19 +0300

    21.04

--- js/script.js
+++ js/script.js
@@ -6,3 +6,8 @@ arr.forEach(function(item, i, arr) {
   console.log(`${i}: ${item} внутри массива ${arr}`);
 });
 
+const str = prompt("", "");
+const product = str.split(',');
+product.sort();
+console.log(product.join('; '));
+
