fa5e3e3c33b02f571e78bedae5e8c28d5a7b76f1
Dimych <example@gmail.com>
2020-07-04 21:59:43 +0300

    26.03

--- js/script.js
+++ js/script.js
@@ -21,3 +21,31 @@ console.log(typeof(Number('4')));
 // 2)
 console.log(typeof(Number(+'5')));
 
+// 3)
+console.log(typeof(parseInt('15 px', 10)));
+
+//let answ = +prompt("Hello", "");
+
+// To boolean
+
+// 0, '',  null, undefained, NaN - false
+
+// 1)
+
+let switcher = null;
+
+if (switcher) {
+  console.log('Working...');
+}
+
+switcher = 1;
+
+if (switcher) {
+  console.log('Working...');
+}
+
+// 2)
+console.log(typeof(Boolean('4')));
+
+// 3)
+console.log(typeof(!!'44444'));
