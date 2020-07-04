5d9891fc1067535c0bc79c3c98077b1d6cbb437e
Dimych <example@gmail.com>
2020-07-04 15:03:55 +0300

    21.03

--- js/script.js
+++ js/script.js
@@ -2,11 +2,7 @@
 
 const arr = [2, 3, 6, 8, 10];
 
-console.log(arr.length);
-
-console.log(arr);
-
-arr[99] = 0;
-console.log(arr.length);
-console.log(arr);
+arr.forEach(function(item, i, arr) {
+  console.log(`${i}: ${item} внутри массива ${arr}`);
+});
 
