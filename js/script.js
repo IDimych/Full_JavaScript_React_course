78448195ada6d7ac69f5508a3eacbca6ea45e7d9
Dimych <example@gmail.com>
2020-07-04 17:15:34 +0300

    22.05

--- js/script.js
+++ js/script.js
@@ -1,61 +1,40 @@
 'use strict';
 
-let a = 5,
-    b = a;
+const oldArr = ['a', 'b', 'c'];
+const newArr = oldArr.slice();
 
-b = b + 5;
+newArr[1] = 'aasdalsj';
+console.log(newArr);
+console.log(oldArr);
 
-console.log (b);
-console.log (a);
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
 
-const obj = {
-  a: 5,
-  b: 1
-};
+console.log(internet);
 
-
-function copy (mainObj) {
-  let objCopy = {};
-
-  let key;
-  for (key in mainObj) {
-    objCopy[key] = mainObj[key];
-  }
-  return objCopy;
+function log (a, b, c) {
+  console.log(a);
+  console.log(b);
+  console.log(c);
 }
 
-const numbers = {
-  a: 2,
-  b: 5,
-  c: {
-    x: 7,
-    y: 4
-  }
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ['a', 'b'];
+
+const newArray = [...array];
+
+const q = {
+  one: 1,
+  two: 2
 };
 
-const newNumbers = copy(numbers);
+const newObj = {...q};
 
-newNumbers.a = 10;
 
-console.log(numbers);
-console.log(newNumbers);
 
-newNumbers.c.x=10;
 
-console.log(numbers);
-console.log(newNumbers);
 
-const add = {
-  d: 17,
-  e: 20
-};
-
-console.log(Object.assign(numbers, add));
-
-const clone = Object.assign({}, add);
-
-console.log(clone);
-
-clone.d = 20;
-
-console.log(clone);
