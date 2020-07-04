d906f7b45355cf1b67d059b5b0cccfca2ce4f01e
Dimych <example@gmail.com>
2020-07-04 13:54:16 +0300

    20.02

--- js/script.js
+++ js/script.js
@@ -21,6 +21,12 @@ console.log(options.name);
 //console.log(options);
 
 for (let key in options) {
-  console.log(`Свойство ${key}, имеет значение ${options[key]}`);
+  if (typeof (options[key]) === 'object') {
+    for (let i in options[key]){
+      console.log(`Свойство ${i}, имеет значение ${options[key][i]}`);
+    }
+  } else {
+    console.log(`Свойство ${key}, имеет значение ${options[key]}`);
+  }
 }
 
