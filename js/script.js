1dac9702e7144937e3f188654bde2986393b6315
Dimych <example@gmail.com>
2020-07-05 01:44:15 +0300

    29.02

--- js/script.js
+++ js/script.js
@@ -6,5 +6,16 @@ const box = document.getElementById('box'),
       hearts = document.querySelectorAll('.heart'),
       oneHeart = document.querySelector('.heart');
 
-box.style.backgroundColor = 'blue';
-box.style.width = '500px';
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+
+const num = 500;
+box.style.cssText = `background-color: blue; width: ${num}px`;
+
+
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
+
+for (let i =0; i < hearts.length; i++) {
+  hearts[i].style.backgroundColor = 'blue';
+}
