acf0e5029b9120bcf60b1bac7bbe6c701b9a309f
Dimych <example@gmail.com>
2020-07-05 14:15:14 +0300

    31.02

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
 const btn = document.querySelector('button');
 
-btn.onclick = function(){
-  alert("Click");
-};
+// btn.onclick = function(){
+//   alert('Click');
+// };
+
+// btn.addEventListener('click', () => {
+//   alert('Click');
+// });
+
+btn.addEventListener('mouseenter', () => {
+  console.log('Hover');
+});
