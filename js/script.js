49e3d12626b9cb1e7e38c75bfdba99a7b3f3224d
Dimych <example@gmail.com>
2020-07-05 12:47:18 +0300

    30.00

--- js/script.js
+++ js/script.js
@@ -1,52 +1,26 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
 'use strict';
 
-const box = document.getElementById('box'),
-      btns = document.getElementsByTagName('button'),
-      circles = document.getElementsByClassName ('circle'),
-      wrapper = document.querySelector('.wrapper'),
-      hearts = wrapper.querySelectorAll('.heart'),
-      oneHeart = wrapper.querySelector('.heart');
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
 
-// box.style.backgroundColor = 'blue';
-// box.style.width = '500px';
-
-const num = 500;
-box.style.cssText = `background-color: blue; width: ${num}px`;
-
-
-btns[1].style.borderRadius = '100%';
-circles[0].style.backgroundColor = 'red';
-
-// for (let i =0; i < hearts.length; i++) {
-//   hearts[i].style.backgroundColor = 'blue';
-// }
-
-hearts.forEach(item => {
-  item.style.backgroundColor = 'blue';
-});
-
-const div = document.createElement('div');
-// const text = document.createTextNode('Тут был я');
-
-div.classList.add('black');
-
-document.body.append(div);
-// document.body.appendChild(div);
-
-// wrapper.append(div);
-// wrapper.prepend(div);
-// hearts[0].before(div);
-// hearts[0].after(div);
-// wrapper.insertBefore(div, hearts[1]);
-// circles[0].remove();
-// wrapper.removeChild(hearts[1]);
-
-hearts[0].replaceWith(circles[0]);
-// wrapper.replaceChild(circles[0], hearts[0]);
-
-div.innerHTML = "<h1>Hello World</h1>";
-
-
-// div.textContent = "Hello World";
-
-div.insertAdjacentHTML('afterend','<h2>Hello</h2>');
