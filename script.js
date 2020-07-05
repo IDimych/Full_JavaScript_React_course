f2674a2230fba4d6f7f58f3f564869080f221acc
Dimych <example@gmail.com>
2020-07-05 14:35:42 +0300

    31.05

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
-const btn = document.querySelector('button');
+const btn = document.querySelector('button'),
+      overlay = document.querySelector('.overlay');
 
 // btn.onclick = function(){
 //   alert('Click');
@@ -9,15 +10,17 @@ const btn = document.querySelector('button');
 //   alert('Click');
 // });
 
-let i = 0;
+// let i = 0;
 const deleteElement = (e) => {
-  console.log(e.target);
-  i++;
-  if (i == 1) {
-    btn.removeEventListener('click', deleteElement);
-  }
+  console.log(e.currentTarget);
+  console.log(e.type);
+  // i++;
+  // if (i == 1) {
+  //   btn.removeEventListener('click', deleteElement);
+  // }
 };
 
 btn.addEventListener('click', deleteElement);
+overlay.addEventListener('click', deleteElement);
 
 
