2e2ab43e631598fcfdb58d270157ab670fb01b72
Dimych <example@gmail.com>
2020-07-05 13:31:44 +0300

    30.01

--- js/script.js
+++ js/script.js
@@ -24,3 +24,27 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+  item.remove();
+});
+
+genre.textContent = 'драма';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+  movieList.innerHTML += `
+      <li class="promo__interactive-item">${i + 1} ${film}
+        <div class="delete"></div>
+      </li>
+  `;
+});
