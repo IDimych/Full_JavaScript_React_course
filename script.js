623e96e45f8267257e9493d9f640d51cca2bfd30
Dimych <example@gmail.com>
2020-07-06 21:57:49 +0300

    39.03

--- script.js
+++ script.js
@@ -2,21 +2,34 @@ const btn = document.querySelector('.btn');
 let timerId,
     i = 0;
 
-btn.addEventListener('click', () => {
-  //const timerId = setTimeout(logger, 2000);
-  timerId = setInterval(logger, 500);
-});
+function MyAnimation() {
+  const elem = document.querySelector('.box');
+  let pos = 0;
 
-function logger() {
-  if (i === 3) {
-    clearInterval(timerId);
+  const id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 300) {
+      clearInterval(id);
+    } else {
+        pos++;
+        elem.style.top = pos + 'px';
+        elem.style.left = pos + 'px';
+    }
   }
-  console.log('text');
-  i++;
 }
 
-let id = setTimeout(function log () {
-  console.log('Hello');
-  id = setTimeout(log, 500);
-}, 500);
+btn.addEventListener('click', MyAnimation);
+
+// function logger() {
+//   if (i === 3) {
+//     clearInterval(timerId);
+//   }
+//   console.log('text');
+//   i++;
+// }
+
+// let id = setTimeout(function log () {
+//   console.log('Hello');
+//   id = setTimeout(log, 500);
+// }, 500);
 
