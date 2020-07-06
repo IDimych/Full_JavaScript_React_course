31669295445d2e15e6f509afc116ed02cf03c032
Dimych <example@gmail.com>
2020-07-06 21:38:56 +0300

    39.01

---
+++ script.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector('.btn');
+let timerId,
+    i = 0;
+
+btn.addEventListener('click', () => {
+  //const timerId = setTimeout(logger, 2000);
+  timerId = setInterval(logger, 500);
+});
+
+function logger() {
+  if (i === 3) {
+    clearInterval(timerId);
+  }
+  console.log('text');
+  i++;
+}
