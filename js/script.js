dce735674f3c81ed6a50d981f31d33a0fb7809cf
Dimych <example@gmail.com>
2020-07-06 20:54:42 +0300

    38.03

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
   function hideTabContent() {
     tabsContent.forEach(item => {
-      item.style.display = 'none';
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
     });
 
     tabs.forEach(item => {
@@ -15,7 +16,8 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
     function showTabContent(i = 0) {
-      tabsContent[i].style.display = 'block';
+      tabsContent[i].classList.add('show', 'fade');
+      tabsContent[i].classList.remove('hide');
       tabs[i].classList.add('tabheader__item_active');
     }
     
