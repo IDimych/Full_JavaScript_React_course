dcb66acc6c1be9dd0a7db6325361e4a7e8aee26d
Dimych <example@gmail.com>
2020-07-07 18:42:26 +0300

    45.02

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 "use strict";
 
-// const num = new Number (3);
-// console.log(num);
+function User(name, id) {
+  this.name = name;
+  this.id = id;
+  this.human = true;
+  this.hello = function() {
+    console.log (`Hello ${this.name}`);
+  };
 
-const num = new Function(3);
-console.log(num);
+}
 
+User.prototype.exit = function() {
+  console.log(`Пользователь ${this.name} ушел`);
+};
+
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
+
+console.log(ivan);
+console.log(alex);
+
+
+ivan.hello();
+alex.hello();
+
+ivan.exit();
+alex.exit();
 
 
