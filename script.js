6bd7daeffec05aa7a2d68b9ff1f07f8dd81199d5
Dimych <example@gmail.com>
2020-07-07 19:14:48 +0300

    46.01

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
 "use strict";
 
-function User(name, id) {
-  this.name = name;
-  this.id = id;
-  this.human = true;
-  this.hello = function() {
-    console.log (`Hello ${this.name}`);
-  };
-
+function showThis() {
+  console.log(this);
 }
 
-User.prototype.exit = function() {
-  console.log(`Пользователь ${this.name} ушел`);
-};
-
-const ivan = new User('Ivan', 28);
-const alex = new User('Alex', 20);
-
-console.log(ivan);
-console.log(alex);
-
-
-ivan.hello();
-alex.hello();
-
-ivan.exit();
-alex.exit();
-
-
+showThis();
