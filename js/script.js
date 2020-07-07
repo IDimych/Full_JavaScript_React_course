8031039412ddc211c1bcfb14a3287786339adbf7
Dimych <example@gmail.com>
2020-07-07 11:48:21 +0300

    44.01

--- js/script.js
+++ js/script.js
@@ -98,12 +98,15 @@ window.addEventListener('DOMContentLoaded', () => {
           modal = document.querySelector('.modal'),
           modalCloseBtn = document.querySelector('[data-close]');
           
+    function openModal () {
+      modal.classList.add('show');
+      modal.classList.remove('hide');
+      document.body.style.overflow = 'hidden';
+      clearInterval(modalTimerId);      
+    }
+
     modalTrigger.forEach(btn => {
-      btn.addEventListener('click', () => {
-        modal.classList.add('show');
-        modal.classList.remove('hide');
-        document.body.style.overflow = 'hidden';
-      });
+      btn.addEventListener('click', openModal);
     });
 
     function closeModal() {
@@ -125,4 +128,15 @@ window.addEventListener('DOMContentLoaded', () => {
         closeModal();
       }
     });
+
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll() {
+      if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+        openModal();
+        window.removeEventListener('scroll', showModalByScroll);
+      }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 });
